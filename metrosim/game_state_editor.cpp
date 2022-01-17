df4a4eda63d8c4b8dc9ee230365ff4527359241f
Chris Whiting <mpro34@gmail.com>
2022-01-17 09:43:25 -0800

    Render the city map and add mouse controls

--- metrosim/game_state_editor.cpp
+++ metrosim/game_state_editor.cpp
@@ -7,7 +7,11 @@ void GameStateEditor::draw(const float dt)
 	if (this->game != nullptr)
 	{
 		this->game->window.clear(sf::Color::Black);
+		this->game->window.setView(this->guiView);
 		this->game->window.draw(this->game->background);
+
+		this->game->window.setView(this->gameView);
+		map.draw(this->game->window, dt);
 	}
 }
 
@@ -34,12 +38,62 @@ void GameStateEditor::handleInput()
 			case sf::Event::Resized:
 			{
 				gameView.setSize(event.size.width, event.size.height);
+				gameView.zoom(zoomLevel);
 				guiView.setSize(event.size.width, event.size.height);
 				this->game->background.setPosition(this->game->window.mapPixelToCoords(sf::Vector2i(0, 0), this->guiView));
 				this->game->background.setScale(
 					float(event.size.width) / float(this->game->background.getTexture()->getSize().x),
 					float(event.size.height) / float(this->game->background.getTexture()->getSize().y)
 				);
+				break;
+			}
+
+			case sf::Event::MouseMoved:
+			{
+				/* Pan the camera */
+				if (this->actionState == ActionState::PANNING)
+				{
+					sf::Vector2f pos = sf::Vector2f(sf::Mouse::getPosition(this->game->window) - this->panningAnchor);
+					gameView.move(-1.0f * pos * this->zoomLevel);
+					panningAnchor = sf::Mouse::getPosition(this->game->window);
+				}
+				break;
+			}
+			case sf::Event::MouseButtonPressed:
+			{
+				/* Start panning */
+				if (event.mouseButton.button == sf::Mouse::Middle)
+				{
+					if (this->actionState != ActionState::PANNING)
+					{
+						this->actionState = ActionState::PANNING;
+						this->panningAnchor = sf::Mouse::getPosition(this->game->window);
+					}
+				}
+				break;
+			}
+			case sf::Event::MouseButtonReleased:
+			{
+				/* Stop panning */
+				if (event.mouseButton.button == sf::Mouse::Middle)
+				{
+					this->actionState = ActionState::NONE;
+				}
+				break;
+			}
+			/* Zoom the view */
+			case sf::Event::MouseWheelMoved:
+			{
+				if (event.mouseWheel.delta < 0)
+				{
+					gameView.zoom(2.0f);
+					zoomLevel *= 2.0f;
+				}
+				else
+				{
+					gameView.zoom(0.5f);
+					zoomLevel *= 0.5f;
+				}
 				break;
 			}
 			default:
@@ -57,4 +111,13 @@ GameStateEditor::GameStateEditor(Game* game)
 	pos *= 0.5f;
 	this->guiView.setCenter(pos);
 	this->gameView.setCenter(pos);
+
+	map = TileMap("city_map.dat", 64, 64, game->tileAtlas);
+	this->zoomLevel = 1.0f;
+
+	/* Centre the camera on the map */
+	sf::Vector2f centre(this->map.width, this->map.height * 0.5);
+	centre *= float(this->map.tileSize);
+	gameView.setCenter(centre);
+	this->actionState = ActionState::NONE;
 }
--- metrosim/game_state_editor.hpp
+++ metrosim/game_state_editor.hpp
@@ -2,11 +2,18 @@
 
 #include <SFML/Graphics.hpp>
 #include "game_state.hpp"
+#include "tile_map.hpp"
+
+enum class ActionState { NONE, PANNING };
 
 class GameStateEditor : public GameState
 {
+	ActionState actionState;
 	sf::View gameView;
 	sf::View guiView;
+	TileMap map;
+	sf::Vector2i panningAnchor;
+	float zoomLevel;
 
 public:
 	virtual void draw(const float dt);
